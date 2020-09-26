32548b7c93904f81f50ab71fdf264012d8f28405
DJWOMS <socanime@gmail.com>
2020-09-26 23:25:04 +0300

    added media

--- config/settings.py
+++ config/settings.py
@@ -122,6 +122,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
@@ -139,7 +142,7 @@ DJOSER = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60*24),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,10 @@
 """pysonet URL Configuration
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+
+from config import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,3 +13,7 @@ urlpatterns = [
     path('auth/', include('djoser.urls.jwt')),
     path('api/v1/', include('src.routers'))
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
