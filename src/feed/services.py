1d63f1ee04485cead483c115a96750601beb1ddf
MironovEvgeny <MironovEvgeny@users.noreply.github.com>
2020-10-12 15:15:53 +0300

    Update of news query

--- src/feed/services.py
+++ src/feed/services.py
@@ -1,21 +1,8 @@
-from django.conf import settings
-from django.contrib.auth.models import User
-from src.followers.models import Follower
 from src.profiles.models import UserNet
 from src.wall.models import Post
 
 
 def feed(user):
-    # 1
-    news = []
-    subscribe = Follower.objects.filter(subscriber=user).values()
-    for sub in subscribe:
-        news.append(Post.objects.filter(user=sub.user, create_date__hour=1).order_by('-create_date'))
-
-    # 2
-    Post.objects.filter(user__id__in=subscribe, create_date__hour=1).order_by('-create_date')
-    user.post.fiter(user__in=subscribe)
-
     # NEW
     # Прямая связь между моделями Follower и Post отсутствует
     # связь организеум через модель User.
@@ -25,23 +12,10 @@ def feed(user):
     # Может быть расскажешь на стриме как развернуть данный проект?
     # с Django и Python только знакомлюсь... сорян если что=)
 
-    # 1 User
-    news = Post.objects.filter(
-        user__in=User.objects.filter(owner__subscriber=user),
-        create_date__hour=1
-    ).order_by('-create_date')
-    print(news.count())
+    # UPD: Запустил и протестил.
 
-    # 2 UserNet
     news = Post.objects.filter(
-        user__in=UserNet.objects.filter(owner__subscriber=user),
-        create_date__hour=1
+        user__in=UserNet.objects.filter(owner__subscriber=user)
     ).order_by('-create_date')
     print(news.count())
-
-    # 2 settings.AUTH_USER_MODEL
-    news = Post.objects.filter(
-        user__in=settings.AUTH_USER_MODEL.objects.filter(owner__subscriber=user),
-        create_date__hour=1
-    ).order_by('-create_date')
-    print(news.count())
+    print(news)
